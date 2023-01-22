da7b6b182713874c478e86c2426e73d898d3dcb0
Сем Елена <semova.s81@bk.ru>
2023-01-22 16:07:13 +0300

    one more made test

--- test/unit/sortByName.test.js
+++ test/unit/sortByName.test.js
@@ -1,11 +1,5 @@
 const sorting = require("../../app");
 
-"coverageThreshold": {
-  "branches": 100,
-  "functions": 100,
-  "lines": 100
-}
-
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
     const input = [
